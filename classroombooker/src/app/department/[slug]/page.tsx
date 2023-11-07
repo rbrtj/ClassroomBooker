66a1b4c16651646dfcbd6ebf8fa5870831eeedfa
rbrtj <robertjaszczurek@icloud.com>
2023-11-07 18:39:16 +0100

    update theme

--- classroombooker/src/app/department/[slug]/page.tsx
+++ classroombooker/src/app/department/[slug]/page.tsx
@@ -1,17 +1,14 @@
 import { notFound } from "next/navigation";
 import { departments } from "~/constants";
-import { api } from "~/trpc/server";
 
 interface PageProps {
   params: {
     slug: string;
   };
 }
-export default async function Home({ params }: PageProps) {
+export default function Home({ params }: PageProps) {
   const { slug } = params;
   if (!departments.find((department) => department.id.toString() === slug))
     return notFound();
-  const scheduleData = await api.schedule.get.query();
-  console.log(scheduleData);
-  return <>{scheduleData.map((data) => data.room)}</>;
+  return <>hi</>;
 }
