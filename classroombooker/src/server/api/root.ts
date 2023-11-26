0d0d34b05649160e2e682d03fedc520baa99fcbd
rbrtj <robertjaszczurek@icloud.com>
2023-11-26 21:13:19 +0100

    exposed rooms query

--- classroombooker/src/server/api/root.ts
+++ classroombooker/src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { buildingRouter } from "./routers/building";
+import { roomsRouter } from "./routers/rooms";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { buildingRouter } from "./routers/building";
 export const appRouter = createTRPCRouter({
   post: postRouter,
   building: buildingRouter,
+  rooms: roomsRouter,
 });
 
 // export type definition of API
