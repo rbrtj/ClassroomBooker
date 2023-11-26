3acd86ca12e825500036a366c026d8a87c6ee8d3
rbrtj <robertjaszczurek@icloud.com>
2023-11-26 21:12:56 +0100

    rooms tanstack query

---
+++ classroombooker/src/server/api/routers/rooms.ts
@@ -0,0 +1,12 @@
+import { createTRPCRouter, publicProcedure } from "../trpc";
+import { z } from "zod";
+
+export const roomsRouter = createTRPCRouter({
+  getRoomsForBuilding: publicProcedure
+    .input(z.object({ buildingId: z.number() }))
+    .query(({ input, ctx }) => {
+      return ctx.db.room.findMany({
+        where: { buildingId: input.buildingId },
+      });
+    }),
+});
