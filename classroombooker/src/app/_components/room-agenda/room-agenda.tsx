e5b55d53a92336bbd6734aef69c4b4cb92a24efd
rbrtj <robertjaszczurek@icloud.com>
2023-12-12 01:04:31 +0100

    Room agenda mobile view

--- classroombooker/src/app/_components/room-agenda/room-agenda.tsx
+++ classroombooker/src/app/_components/room-agenda/room-agenda.tsx
@@ -73,7 +73,7 @@ const LectureRow = React.memo(
 
 LectureRow.displayName = "LectureRow";
 
-export function RoomAgenda({ agenda }: RoomAgendaProps) {
+const DefaultRoomAgenda = ({ agenda }: RoomAgendaProps) => {
   return (
     <div className="w-full overflow-x-scroll rounded-lg bg-white p-8 shadow-md md:overflow-x-visible">
       <Table className="w-full min-w-[600px] overflow-hidden rounded-lg md:min-w-0">
@@ -91,5 +91,51 @@ export function RoomAgenda({ agenda }: RoomAgendaProps) {
         </TableBody>
       </Table>
     </div>
+  );
+};
+
+const MobileRoomAgenda = ({ agenda }: RoomAgendaProps) => {
+  return (
+    <div className="w-full overflow-x-scroll rounded-lg bg-white p-8 shadow-md md:overflow-x-visible">
+      <Table className="w-full rounded-lg md:min-w-0">
+        {DAYS_OF_WEEK.map((day) => (
+          <>
+            <TableHeader>
+              <TableRow>
+                <TableHead>{day}</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {CLASSES_HOURS.map((lecture) => (
+                <TableRow key={lecture.startTime}>
+                  <TableCell>
+                    {lecture.startTime} - {lecture.endTime}
+                  </TableCell>
+                  <AgendaCell
+                    key={day}
+                    agenda={agenda}
+                    lecture={lecture}
+                    day={day}
+                  />
+                </TableRow>
+              ))}
+            </TableBody>
+          </>
+        ))}
+      </Table>
+    </div>
+  );
+};
+
+export function RoomAgenda({ agenda }: RoomAgendaProps) {
+  return (
+    <>
+      <div className="h-full md:hidden">
+        <MobileRoomAgenda agenda={agenda} />
+      </div>
+      <div className="container hidden w-full md:block">
+        <DefaultRoomAgenda agenda={agenda} />
+      </div>
+    </>
   );
 }
