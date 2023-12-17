76be37da2e2c2842c95921177ac1a828e347f076
rbrtj <robertjaszczurek@icloud.com>
2023-12-18 00:02:27 +0100

    Removed unused post router

--- classroombooker/src/server/api/root.ts
+++ classroombooker/src/server/api/root.ts
@@ -1,4 +1,3 @@
-import { postRouter } from "~/server/api/routers/post";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { buildingRouter } from "./routers/building";
 import { roomsRouter } from "./routers/rooms";
@@ -11,7 +10,6 @@ import { teacherRouter } from "./routers/teacher";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
   building: buildingRouter,
   rooms: roomsRouter,
   lectures: lecturesRouter,
