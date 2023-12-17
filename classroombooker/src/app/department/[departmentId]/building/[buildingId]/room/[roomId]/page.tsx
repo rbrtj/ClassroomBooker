137215a097e13effbbea499ed8bc0726980c5de4
rbrtj <robertjaszczurek@icloud.com>
2023-12-18 00:00:41 +0100

    roomId page -> client side

--- classroombooker/src/app/department/[departmentId]/building/[buildingId]/room/[roomId]/page.tsx
+++ classroombooker/src/app/department/[departmentId]/building/[buildingId]/room/[roomId]/page.tsx
@@ -1,5 +1,6 @@
+"use client";
 import { RoomAgenda } from "~/components/RoomAgenda";
-import { api } from "~/trpc/server";
+import { api } from "~/trpc/react";
 
 interface PageProps {
   params: {
@@ -7,9 +8,19 @@ interface PageProps {
   };
 }
 
-export default async function Home({ params }: PageProps) {
+export default function Home({ params }: PageProps) {
   const { roomId } = params;
   // TODO: Add error handling for invalid roomId (e.g. string)
-  const agenda = await api.agenda.getAgenda.query({ roomId: parseInt(roomId) });
-  return <RoomAgenda agenda={agenda} />;
+  const { data: agenda, refetch } = api.lectures.getLectures.useQuery({
+    roomId: parseInt(roomId),
+  });
+
+  // Used to omit refetch type
+  const refetchWrapper = async () => {
+    await refetch();
+  };
+
+  if (!agenda) return null;
+
+  return <RoomAgenda agenda={agenda} refetchLectures={refetchWrapper} />;
 }
