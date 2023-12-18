9ee9793d068e0787d5bfb054efbb355fb259927b
rbrtj <robertjaszczurek@icloud.com>
2023-12-18 01:06:58 +0100

    Add lecture mutation

---
+++ classroombooker/src/server/api/routers/lectures.ts
@@ -0,0 +1,71 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+import { doTimesOverlap } from "~/utils/DoTimesOverlap";
+import { TRPCError } from "@trpc/server";
+
+export const lecturesRouter = createTRPCRouter({
+  getLectures: publicProcedure
+    .input(z.object({ roomId: z.number() }))
+    .query(({ input, ctx }) => {
+      return ctx.db.lecture.findMany({
+        where: { roomId: input.roomId },
+      });
+    }),
+  deleteLecture: publicProcedure
+    .input(z.object({ id: z.number() }))
+    .mutation(({ input, ctx }) => {
+      return ctx.db.lecture.delete({
+        where: { id: input.id },
+      });
+    }),
+  addLecture: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        roomId: z.number(),
+        teacherId: z.number(),
+        dayOfWeek: z.enum([
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+        ]),
+        evenWeek: z.boolean(),
+        type: z.enum(["lecture", "seminar", "other", "practice"]),
+        startTime: z.string(),
+        endTime: z.string(),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const schedule = await ctx.db.lecture.findMany({
+        where: { roomId: input.roomId, dayOfWeek: input.dayOfWeek },
+      });
+      const isOverlapping = schedule.some((lecture) =>
+        doTimesOverlap(
+          lecture.startTime,
+          lecture.endTime,
+          input.startTime,
+          input.endTime,
+        ),
+      );
+      if (isOverlapping) {
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Lecture overlaps with another lecture",
+        });
+      }
+      return ctx.db.lecture.create({
+        data: {
+          name: input.name,
+          roomId: input.roomId,
+          teacherId: input.teacherId,
+          dayOfWeek: input.dayOfWeek,
+          evenWeek: input.evenWeek,
+          type: input.type,
+          startTime: input.startTime,
+          endTime: input.endTime,
+        },
+      });
+    }),
+});
